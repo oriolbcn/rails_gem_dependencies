464b384f2afeda73ec48504df8316551893516cf
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-01-03 00:14:39 +0100

    adapted

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-name              "rails_gem_dependencies-tlq"
-maintainer        "Ben Dixon"
-maintainer_email  "ben@hillsbede.co.uk"
+name              "rails_gem_dependencies"
+maintainer        "Oriol Collell"
+maintainer_email  "ocollell@dineyo.com"
 description       "Installs packages commonly required by rails gems"
 version           "0.0.1"
 
-recipe "rails_gem_dependencies-tlq", "all the gems"
+recipe "rails_gem_dependencies", "all the gems"
 
 supports "ubuntu"
