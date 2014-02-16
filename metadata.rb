b1e3e4759fd84ea639cd4694cf96bc31aa43ae5a
Oriol Collell <oriol.collell.bcn@gmail.com>
2014-02-16 23:21:12 +0100

    added libpq-dev, necessary for pg gem

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name              "rails_gem_dependencies"
 maintainer        "Oriol Collell"
 maintainer_email  "ocollell@dineyo.com"
 description       "Installs packages commonly required by rails gems"
-version           "0.0.1"
+version           "0.0.2"
 
 recipe "rails_gem_dependencies", "all the gems"
 
